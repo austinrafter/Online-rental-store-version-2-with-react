0e7f7253957896f9d2645fca65f770b51f07e774
austinrafter <56013277+austinrafter@users.noreply.github.com>
2020-03-30 16:08:03 -0700

    new imports and routes
    new file imports and routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import About from './components/howitworks';
 import Navbar from './components/CustomNavbar'
 import SignIn from './components/signIn'
 import SignUp from './components/signUp'
+import SearchCar from './components/startRes'
 import Checkout from './components/checkout'
+import List from './components/list'
 
 class App extends Component {
   render () {
@@ -18,10 +20,11 @@ class App extends Component {
       <Navbar />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/startres" component={SearchCar} />
       <Route exact path="/SignIn" component={SignIn} />
       <Route exact path="/SignUp" component={SignUp} />
       <Route exact path="/checkout" component={Checkout} />
-
+      <Route exact path="/list" component={List} />
       </div>
       </Router>
     );
