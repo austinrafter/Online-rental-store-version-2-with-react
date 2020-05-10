989b5cb079fddd72d4bab29a734a48dc5f320738
austinrafter <56013277+austinrafter@users.noreply.github.com>
2020-05-10 10:28:20 -0700

    Update CustomNavbar.jsx

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -3,15 +3,16 @@ import {Navbar, Nav} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import './CustomNavbar.css';
 
-
+/*Austin Rafter*/
 export default class CustomNavbar extends Component {
 
   render(){
     return(
+      /*Austin Rafter*/
       <Navbar bg="primary" variant="light">
      <Navbar.Brand><Link to="/" className="brand-logo"><strong>EZ-PZ</strong></Link></Navbar.Brand>
+       
      <Nav className="mr-auto">
-
        <Nav.Link><Link to ="/" to="/"><strong>Home</strong></Link></Nav.Link>
        <Nav.Link><Link to="/about"><strong>How It Works</strong></Link></Nav.Link>
        <Nav.Link><Link to="/Cars"><strong>Start Reservation</strong></Link></Nav.Link>
